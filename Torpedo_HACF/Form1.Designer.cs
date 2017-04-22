6779d930de63382a4d35b1b50a124b60e0d99a50
delld <delld@DESKTOP-MPFCH73>
2017-04-22 13:57:41 +0200

    Final

--- Torpedo_HACF/Form1.Designer.cs
+++ Torpedo_HACF/Form1.Designer.cs
@@ -28,20 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.gepdgv = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gepdgv)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(232, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(401, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tábla szélesség";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(412, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hajók száma";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(45, 135);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(334, 268);
+            this.dgv.TabIndex = 7;
+            // 
+            // gepdgv
+            // 
+            this.gepdgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gepdgv.Location = new System.Drawing.Point(484, 135);
+            this.gepdgv.Name = "gepdgv";
+            this.gepdgv.RowTemplate.Height = 24;
+            this.gepdgv.Size = new System.Drawing.Size(357, 268);
+            this.gepdgv.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(153, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Játékos táblája";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(625, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Gép Tábla";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 454);
+            this.ClientSize = new System.Drawing.Size(912, 476);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gepdgv);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Torpedó";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gepdgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridView gepdgv;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Torpedo_HACF/Form1.cs
+++ Torpedo_HACF/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace Torpedo_HACF
 {
@@ -16,7 +17,304 @@ namespace Torpedo_HACF
         {
             InitializeComponent();
         }
+        //Tábla
+        public int THOSSZ, HajoDb;
+        public string[,] tabla;
+        public string[,] geptabla;
+        struct Hajo
+        {
+            public int balfelsox;
+            public int balfelsoy;
+            public int hossz;
+            public bool irany;
+            //igaz - vizszintes
+            //hamis fuggolege
 
-        //Xyc
+        }
+        
+
+        public int convertColToNum(char C)
+        {
+            //65-90 -> -64
+            return (C) - 65;
+        }
+
+        List<Hajo> hajok = new List<Hajo>();
+        List<Hajo> gepihajok = new List<Hajo>();
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int th, tsz, hdb;
+            if (int.TryParse(textBox1.Text, out th) && int.TryParse(textBox3.Text, out tsz) && int.TryParse(textBox3.Text, out hdb))
+            {
+                THOSSZ = th;
+                HajoDb = hdb;
+
+                tabla = new string[THOSSZ, THOSSZ];
+                geptabla = new string[THOSSZ, THOSSZ];
+                for (int i = 0; i < THOSSZ; i++)
+                {
+                    for (int j = 0; j < THOSSZ; j++)
+                    {
+                        tabla[i, j] = "-";
+                        geptabla[i, j] = "-";
+                    }
+                }
+
+                while (hajok.Count != HajoDb)
+                {
+                    Hajo s = new Hajo();
+                    Hajo g = new Hajo();
+                    string bemenet = Interaction.InputBox("Add meg a hajó adatait, a követkető formátumban: Kordináta,hossz,F/V");
+
+                    string[] seged = bemenet.Split(',');
+                    string kord = seged[0];
+                    int x = convertColToNum(kord[0]);
+                    int y = int.Parse(kord[1].ToString());
+                    y--;
+                    s.balfelsox = x;
+                    s.balfelsoy = y;
+
+
+                    s.hossz = int.Parse(seged[1]);
+                    if (s.hossz > THOSSZ - 1)
+                    {
+                        MessageBox.Show("Hubás bemenet");
+                        continue;
+                    }
+                    s.irany = seged[2] == "V";
+                    if (s.irany)
+                    {
+                        if (x + s.hossz > THOSSZ)
+                        {
+                            MessageBox.Show("Hubás bemenet");
+                            continue;
+                        }
+
+                    }
+                    else
+                    {
+                        if (y + s.hossz > THOSSZ)
+                        {
+                            MessageBox.Show("Hubás bemenet");
+                            continue;
+                        }
+                    }
+             
+                    
+                    if (!Lehet(tabla,s))
+                    {
+                        MessageBox.Show("Hubás bemenet");
+                        continue;
+                    }
+
+                    if (s.irany)
+                    {
+                        for (int i = x; i < x+s.hossz; i++)
+                        {
+                            tabla[i, y] = "h";
+                        }
+                    }
+                    else
+                    {
+                        for (int i = y; i < y + s.hossz; i++)
+                        {
+                            tabla[x, i] = "h";
+                        }
+                    }
+
+                    g.hossz = s.hossz;
+                    g.irany = s.irany;
+                    bool jo = false;
+                    Random r = new Random();
+                    while (!jo)
+                    {
+                        g.balfelsox = 0;
+                        g.balfelsoy = 0;
+                        int xgep = r.Next(0,THOSSZ);
+                        int ygep = r.Next(0, THOSSZ);
+
+                        g.balfelsox = xgep;
+                        g.balfelsoy = ygep;
+
+
+                        if (g.irany)
+                        {
+                            if (xgep + g.hossz > THOSSZ)
+                            {
+                                continue;
+                            }
+
+                        }
+                        else
+                        {
+                            if (ygep + g.hossz > THOSSZ)
+                            {
+                                continue;
+                            }
+                        }
+
+                        if (!Lehet(geptabla, g))
+                        {
+                            continue;
+                        }
+
+                        jo = true;
+                        if (g.irany)
+                        {
+                            for (int i = xgep; i < xgep + g.hossz; i++)
+                            {
+                                geptabla[i, ygep] = "h";
+                            }
+                        }
+                        else
+                        {
+                            for (int i = ygep; i < ygep + s.hossz; i++)
+                            {
+                                geptabla[xgep, i] = "h";
+                            }
+                        }
+
+                    }
+
+                    gepihajok.Add(g);
+                    hajok.Add(s);
+                }
+
+
+                dgv.RowCount = THOSSZ;
+                dgv.ColumnCount = THOSSZ;
+                gepdgv.RowCount = THOSSZ;
+                gepdgv.ColumnCount = THOSSZ;
+
+                for (int i = 0; i < THOSSZ; i++)
+                {
+                    for (int j = 0; j < THOSSZ; j++)
+                    {
+                        dgv.Rows[i].Cells[j].Value = tabla[i, j];
+                    }
+                }
+
+                dgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+                dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                gepdgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+                gepdgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+
+
+            }
+
+         
+        }
+
+
+        //REST API
+        private bool Lehet(string[,] tabla, Hajo h)
+        {
+            if (h.irany)
+            {
+                for (int i = h.balfelsox; i <h.balfelsox+h.hossz; i++)
+                {
+                    if (tabla[i,h.balfelsoy] != "-")
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = h.balfelsoy; i < h.balfelsoy + h.hossz; i++)
+                {
+                    if (tabla[i, h.balfelsox] != "-")
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        string getC(int X, int Y, string[,] TABLA, List<Hajo> hajok)
+        {
+            if (TABLA[X, Y] == "-")
+                return "0";
+            if (TABLA[X, Y] == "x")
+                return "1";
+            if (TABLA[X, Y] == "h")
+                return "0";
+            if (TABLA[X, Y] == "hx")
+            {
+                Hajo hajo = hajok.First(x => X <= x.balfelsox + x.hossz && X >= x.balfelsox && Y <= x.balfelsoy + x.hossz && Y >= x.balfelsoy);
+                if (hajo.irany) //vizszintes
+                {
+                    if (hajo.hossz == 1)
+                    {
+                        if (TABLA[Y, X] == "hx")
+                            return "3";
+                        else
+                            return "2";
+                    }
+                    else
+                    {
+                        int hcx = 0;
+                        for (int x = hajo.balfelsox; x < hajo.balfelsox + hajo.hossz; x++)
+                        {
+                            if (TABLA[hajo.balfelsoy, x] == "hx")
+                                hcx++;
+                        }
+                        if (hcx == hajo.hossz)
+                            return "3";
+                        else
+                            return "2";
+                    }
+                }
+                else //fuggoleges
+                {
+                    if (hajo.hossz == 1)
+                    {
+                        if (TABLA[Y, X] == "hx")
+                            return "3";
+                        else
+                            return "2";
+                    }
+                    else
+                    {
+                        int hcy = 0;
+                        for (int y = hajo.balfelsoy; y < hajo.balfelsoy + hajo.hossz; y++)
+                        {
+                            if (TABLA[y, hajo.balfelsox] == "hx")
+                            {
+                                hcy++;
+                            }
+                        }
+                        if (hcy == hajo.hossz)
+                            return "3";
+                        else
+                            return "2";
+                    }
+                }
+            }
+            return "";
+        }
+
+        public string ConvertTableToREST(string[,] rest, int o)
+        {
+            string op = "";
+            for (int i = 0; i < o; i++)
+            {
+                for (int j = 0; j < o; j++)
+                {
+                    op += getC(i, j, rest, hajok);
+                }
+            }
+            return op;
+        }
+
+        
     }
 }
